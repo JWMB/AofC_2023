57b654e94dcacfc10ec9e8e40eaa7d8062c78da7
JWMB <jonas.beckeman@gmail.com>
2023-12-01 14:49:27 +0100

    Day 1

--- AofC_2023.Tests/Tests.fs
+++ AofC_2023.Tests/Tests.fs
@@ -14,9 +14,24 @@ let ``template`` () =
 [<Fact>]
 let ``D1`` () =
     let input = """
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
 """
     let pt1 = D1.part1 input
-    Assert.Equal(24000, pt1)
+    Assert.Equal(142, pt1)
     
-    //let pt2 = D1.part2 input
-    //Assert.Equal(45000, pt2)
+    Assert.Equal(28, D1.part2 "two1nineight")
+
+    let input2 = """
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+"""
+    let pt2 = D1.part2 input2
+    Assert.Equal(281, pt2)
--- AofC_2023/Days/D1.fs
+++ AofC_2023/Days/D1.fs
@@ -1,7 +1,52 @@
 ﻿module D1
 
 open Tools
+open System.Text.RegularExpressions
+
+let firstLast arr =
+    let len = arr |> Array.length
+    let firstLastX = 
+        match len with
+             | 0 -> "0"
+             | _ -> $"{arr[0]}{arr[len - 1]}" // WTF? works in debugger but not in code!? $"{digits[0]}{digits[^1]}"
+        
+    firstLastX |> int
+
 
 let part1 input =
-    let result = 1
-    result
+    let parseRow input =
+        let digits = Regex.Replace(input, @"\D", "") |> Seq.toArray
+        firstLast digits
+        //let firstLastX = 
+        //    match digits.Length with
+        //     | 0 -> "0"
+        //     | _ -> $"{digits[0]}{digits[digits.Length - 1]}" // WTF? works in debugger but not in code!? $"{digits[0]}{digits[^1]}"
+        //firstLastX |> int
+
+    let rowValues = Parsing.parseRows input parseRow
+    Seq.sum rowValues
+
+let part2 input =
+    let digitWords = "zero one two three four five six seven eight nine".Split ' '
+    let digitWordPattern = digitWords |> String.concat "|"
+    let pattern = $@"({digitWordPattern}|\d)"
+
+    let makeDigit str =
+        match digitWords |> Array.tryFindIndex (fun f -> f = str) with
+            | Some s -> s
+            | _ -> int str
+
+    let parseRow input =
+        let optionalFinal = Regex.Match(input, $"{pattern}$")
+        let matches = Regex.Matches(input, pattern) |> Seq.toArray
+        let matches = 
+            if optionalFinal.Success && optionalFinal.Value <> (matches |> Array.last).Value then
+                [| optionalFinal |] |> Array.append matches
+            else
+                matches
+        let values = matches |> Array.map (fun f -> makeDigit(f.Value))
+        firstLast values
+
+    let rowValues = Parsing.parseRows input parseRow
+    let result = Seq.sum rowValues
+    result // 55309 too high
